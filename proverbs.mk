687e831ca384d0d5a753c3bc2d0f3b4523badfe9
ajiehust <ajiehust@gmail.com>
2024-06-22 17:39:57 +0200

    Create proverbs.mk

---
+++ proverbs.mk
@@ -0,0 +1,6 @@
+# 胡适曾经说一个人愚昧有四大表现
+ - 对从未见过的人恨之入骨 "Hass auf jemanden, den man nie gesehen hat" 
+ - 对从未做过的事引以为豪 "Stolz auf etwas, das man noch nie getan hat" 
+ - 从吹捧出来的神倒头就拜 "Einen Götzen anbeten, den man selbst gemacht hat". 
+ - 对画未得的饼感恩戴德 "Dankbar für das Brot, das man noch nicht gebacken hat"
+ 
